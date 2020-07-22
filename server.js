d7f48798bef8db23248c0270bc51686c40e39761
Hasan Nadeem <64611079+hasan-itsol-2020@users.noreply.github.com>
2020-07-22 23:16:51 +0500

    Project Completed v2.0

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const connectDB = require('./config/db')
+const path = require('path')
 
 const app = express()
 const port = process.env.PORT || 5000
@@ -7,14 +8,17 @@ app.use(express.json())
 
 connectDB()
 
-app.get('/', (req, res) => {
-    res.send('App Running')
-})
-
 app.use('/api/users', require('./routes/api/users'))
 app.use('/api/profile', require('./routes/api/profile'))
 app.use('/api/auth', require('./routes/api/auth'))
 app.use('/api/posts', require('./routes/api/posts'))
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'))
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`)
